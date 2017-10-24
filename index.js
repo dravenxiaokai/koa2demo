2282b35fc30c0ef6a1f5adaeaf260f15c4b57658
dravenxiaokai <jspankai@vip.qq.com>
2017-10-25 00:34:29 +0800

    koa@2 koa-views中间件加载模板引擎

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
 const Koa = require('koa')
-const session = require('koa-session-minimal')
-const MysqlSession = require('koa-mysql-session')
+const views = require('koa-views')
+const path = require('path')
 
 const app = new Koa()
 
-//配置存储session信息的mysql
-let store = new MysqlSession({
-    user: 'root',
-    password: 'root',
-    database: 'koademo',
-    host: '127.0.0.1'
-})
-
-//存放sessionId的cookie配置
-let cookie = {
-    maxAge: '',
-    expires: '',
-    path: '',
-    domain: '',
-    httpOnly: '',
-    overwrite: '',
-    secure: '',
-    sameSite: '',
-    signed: ''
-}
-
-//使用session中间件
-app.use(session({
-    key: 'SESSION_ID',
-    store: store,
-    cookie: cookie
+//加载模板引擎
+app.use(views(path.join(__dirname, './views'), {
+    extension: 'ejs'
 }))
 
 app.use(async (ctx) => {
-    //设置session
-    if (ctx.url === '/set') {
-        ctx.session = {
-            user_id: Math.random().toString(36).substr(2),
-            count: 0
-        }
-        ctx.body = ctx.session
-    } else if (ctx.url === '/') {
-        //读取session信息
-        ctx.session.count = ctx.session.count + 1
-        ctx.body = ctx.session
-    }
+    let title = 'hello koa@2'
+    await ctx.render('index', {
+        title
+    })
 })
 
 app.listen(3000, () => {
-    console.log('[demo] session is starting on port: 3000')
+    console.log('[demo] koa-views ejs is starting on port: 3000')
 })
