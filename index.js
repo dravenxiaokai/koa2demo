a4681991c2d9ab482b49c096f152693f27814136
dravenxiaokai <jspankai@vip.qq.com>
2017-10-24 23:44:57 +0800

    koa@2 使用cookie

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const Koa = require('koa')
-const path = require('path')
-const static = require('koa-static')
-
 const app = new Koa()
 
-//静态资源相对路径
-const staticPath = './public'
-
-app.use(static(path.join(__dirname, staticPath)))
+app.use(async (ctx) => {
+    if (ctx.url === '/index') {
+        ctx.cookies.set('cid', 'hello world', {
+            domain: 'localhost',//cookie所在的域名
+            path: '/index',//cookie所在的路径
+            maxAge: 20 * 60 * 1000,//cookie有效时长
+            expires: new Date('2018-10-24'),//cookie失效时间
+            httpOnly: false,//是否只用于http请求中获取
+            overwrite: false//是否允许重写
+        })
+        ctx.body = 'cookie is ok'
+    } else {
+        ctx.body = 'hello koa@2'
+    }
+})
 
 app.use(async (ctx) => {
     ctx.body = 'hello koa@2'
 })
 
 app.listen(3000, () => {
-    console.log('[demo] koa-static middleware is starting on port: 3000')
+    console.log('[demo] cookie is starting on port: 3000')
 })
