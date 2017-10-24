04782700d23a2ec236b57fcaf2c585cb055cc220
dravenxiaokai <jspankai@vip.qq.com>
2017-10-24 23:23:44 +0800

    koa@2 koa-static中间件

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 const Koa = require('koa')
-const app = new Koa()
-const bodyParser = require('koa-bodyparser')
+const path = require('path')
+const static = require('koa-static')
 
-//使用ctx.body解析中间件
-app.use(bodyParser())
+const app = new Koa()
+
+//静态资源相对路径
+const staticPath = './public'
+
+app.use(static(path.join(__dirname, staticPath)))
 
 app.use(async (ctx) => {
-    if (ctx.url === '/' && ctx.method === 'GET') {
-        //get请求时返回表单
-        let html = `
-            <h2>koa@2 post request</h2>
-            <form action="/" method="POST">
-                <p>userName</p>
-                <input name="username" type="text"><br>
-                <p>userPwd</p>
-                <input name="userPwd" type="password"><br>
-                <button type="submit">submit</button>
-            </form>
-        `
-        ctx.body = html
-    } else if (ctx.url === '/' && ctx.method === 'POST') {
-        //post请求时，解析表单里数据，并显示
-        let postData = ctx.request.body
-        ctx.body = postData
-    } else {
-        //其他请求显示404
-        ctx.body = '<h1>404 page</h1>'
-    }
+    ctx.body = 'hello koa@2'
 })
 
 app.listen(3000, () => {
-    console.log('[demo] koa-bodyparser is starting on port: 3000')
+    console.log('[demo] koa-static middleware is starting on port: 3000')
 })
