b1ad28152e4581d634376e67c59f8405ff48ad6e
dravenxiaokai <jspankai@vip.qq.com>
2017-10-25 02:08:35 +0800

    koa@2 异步上传图片

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 const Koa = require('koa')
+const views = require('koa-views')
 const path = require('path')
-const app = new Koa()
-// const bodyParser = require('koa-bodyparser')
+const convert = require('koa-convert')
+const static = require('koa-static')
 const { uploadFile } = require('./util/upload')
-// app.use(bodyParser()) 
+const app = new Koa()
+
+/**
+* 使用第三方中间件 start 
+*/
+app.use(views(path.join(__dirname, './views'), {
+    extension: 'ejs'
+}))
+
+// 静态资源目录对于相对入口文件index.js的路径 
+const staticPath = './public'
+// 由于koa-static目前不支持koa2 
+// 所以只能用koa-convert封装一下 
+app.use(convert(static(path.join(__dirname, staticPath))))
+
+/**
+* 使用第三方中间件 end 
+*/
 app.use(async (ctx) => {
-    if (ctx.url === '/' && ctx.method === 'GET') {
-        // 当GET请求时候返回表单页面
-        let html = `
-            <h1>koa2 upload demo</h1>
-            <form method="POST" action="/upload.json" enctype="multipart/form-data">
-            <p>file upload</p>
-            <span>picName:</span><input name="picName" type="text" /><br/>
-            <input name="file" type="file" /><br/><br/>
-            <button type="submit">submit</button>
-            </form>
-        `
-        ctx.body = html
-    } else if (ctx.url === '/upload.json' && ctx.method === 'POST') {
-        // 上传文件请求处理
-        let result = {
-            success: false
-        }
-        let serverFilePath = path.join(__dirname, 'upload-files')
-        // 上传文件事件
+    if (ctx.method === 'GET') {
+        let title = 'upload pic async'
+        await ctx.render('index', {
+            title,
+        })
+    }
+    else if (ctx.url === '/api/picture/upload.json' && ctx.method === 'POST') {
+        // 上传文件请求处理 
+        let result = { success: false }
+        let serverFilePath = path.join(__dirname, 'public/image')
+        // 上传文件事件 
         result = await uploadFile(ctx, {
-            fileType: 'album', // common or album 
+            fileType: 'album',
             path: serverFilePath
         })
         ctx.body = result
     } else {
-        // 其他请求显示404 
+        // 其他请求显示404
         ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
     }
 })
 
 app.listen(3000, () => {
-    console.log('[demo] upload-simple is starting at port 3000')
+    console.log('[demo] upload-async is starting at port 3000')
 })
