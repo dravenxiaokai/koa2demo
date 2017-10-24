31b242c9e9c82bb4306e57b3eb96a48085adef05
dravenxiaokai <jspankai@vip.qq.com>
2017-10-25 00:18:22 +0800

    koa@2 实现session

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 const Koa = require('koa')
+const session = require('koa-session-minimal')
+const MysqlSession = require('koa-mysql-session')
+
 const app = new Koa()
 
+//配置存储session信息的mysql
+let store = new MysqlSession({
+    user: 'root',
+    password: 'root',
+    database: 'koademo',
+    host: '127.0.0.1'
+})
+
+//存放sessionId的cookie配置
+let cookie = {
+    maxAge: '',
+    expires: '',
+    path: '',
+    domain: '',
+    httpOnly: '',
+    overwrite: '',
+    secure: '',
+    sameSite: '',
+    signed: ''
+}
+
+//使用session中间件
+app.use(session({
+    key: 'SESSION_ID',
+    store: store,
+    cookie: cookie
+}))
+
 app.use(async (ctx) => {
-    if (ctx.url === '/index') {
-        ctx.cookies.set('cid', 'hello world', {
-            domain: 'localhost',//cookie所在的域名
-            path: '/index',//cookie所在的路径
-            maxAge: 20 * 60 * 1000,//cookie有效时长
-            expires: new Date('2018-10-24'),//cookie失效时间
-            httpOnly: false,//是否只用于http请求中获取
-            overwrite: false//是否允许重写
-        })
-        ctx.body = 'cookie is ok'
-    } else {
-        ctx.body = 'hello koa@2'
+    //设置session
+    if (ctx.url === '/set') {
+        ctx.session = {
+            user_id: Math.random().toString(36).substr(2),
+            count: 0
+        }
+        ctx.body = ctx.session
+    } else if (ctx.url === '/') {
+        //读取session信息
+        ctx.session.count = ctx.session.count + 1
+        ctx.body = ctx.session
     }
 })
 
-app.use(async (ctx) => {
-    ctx.body = 'hello koa@2'
-})
-
 app.listen(3000, () => {
-    console.log('[demo] cookie is starting on port: 3000')
+    console.log('[demo] session is starting on port: 3000')
 })
