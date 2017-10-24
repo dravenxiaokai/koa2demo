3db4f47db7f83b25e93573cc23d47f092b9655b7
dravenxiaokai <jspankai@vip.qq.com>
2017-10-25 01:20:36 +0800

    koa@2 文件上传简单实现

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const inspect = require('util').inspect
+const Koa = require('koa')
 const path = require('path')
-const fs = require('fs')
-const Busboy = require('busboy')
-
-//req为node原生请求
-const busboy = new Busboy({ headers: req.headers })
-
-//监听文件解析事件
-busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-    console.log(`File [${fieldname}]: filename: ${filename}`)
-
-    //文件保存特定路径
-    file.pipe(fs.createWriteStream('./upload'))
-
-    //开始解析文件流
-    file.on('data', (data) => {
-        console.log(`File [${fieldname}] got ${data.length} bytes`)
-    })
-
-    //解析文件结束
-    file.on('end', () => {
-        console.log(`File [${fieldname}] finished`)
-    })
+const app = new Koa()
+// const bodyParser = require('koa-bodyparser')
+const { uploadFile } = require('./util/upload')
+// app.use(bodyParser()) 
+app.use(async (ctx) => {
+    if (ctx.url === '/' && ctx.method === 'GET') {
+        // 当GET请求时候返回表单页面
+        let html = `
+            <h1>koa2 upload demo</h1>
+            <form method="POST" action="/upload.json" enctype="multipart/form-data">
+            <p>file upload</p>
+            <span>picName:</span><input name="picName" type="text" /><br/>
+            <input name="file" type="file" /><br/><br/>
+            <button type="submit">submit</button>
+            </form>
+        `
+        ctx.body = html
+    } else if (ctx.url === '/upload.json' && ctx.method === 'POST') {
+        // 上传文件请求处理
+        let result = {
+            success: false
+        }
+        let serverFilePath = path.join(__dirname, 'upload-files')
+        // 上传文件事件
+        result = await uploadFile(ctx, {
+            fileType: 'album', // common or album 
+            path: serverFilePath
+        })
+        ctx.body = result
+    } else {
+        // 其他请求显示404 
+        ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+    }
 })
 
-//监听请求中的字段
-busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated) => {
-    console.log(`Field [${fieldname}]: value: ${inspect(val)}`)
+app.listen(3000, () => {
+    console.log('[demo] upload-simple is starting at port 3000')
 })
-
-//监听结束事件
-busboy.on('finish', () => {
-    console.log('Done parsing form!')
-    res.writeHead(303, { Connection: 'close', Location: '/' })
-    res.end()
-})
-req.pipe(busboy)
