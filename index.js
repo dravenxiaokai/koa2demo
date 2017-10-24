19b854589cb2d2e645ae3680ed80e71edbd7d948
dravenxiaokai <jspankai@vip.qq.com>
2017-10-24 22:20:24 +0800

    koa@2 GET请求获取数据

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const Koa = require('koa')
-const fs = require('fs')
 const app = new Koa()
 
-const Router = require('koa-router')
+app.use(async (ctx)=>{
+    let url = ctx.url
+    //从上下文的request对象中获取
+    let request = ctx.request
+    let req_query = request.query
+    let req_querystring = request.querystring
 
-//子路由1
-let home = new Router()
-home.get('/', async (ctx) => {
-    let html = `
-        <ul>
-            <li><a href="/page/helloworld">/page/helloworld</a></li>
-            <li><a href="/page/404">/page/404</a></li>
-        </ul>
-    `
-    ctx.body = html
+    //从上下文直接获取
+    let ctx_query = ctx.query
+    let ctx_querystring = ctx.querystring
+
+    ctx.body = {
+        url,
+        req_query,
+        req_querystring,
+        ctx_query,
+        ctx_querystring
+    }
 })
 
-//子路由2
-let page = new Router()
-page
-    .get('/404', async (ctx) => {
-        ctx.body = '404 page'
-    })
-    .get('/helloworld', async (ctx) => {
-        ctx.body = 'helloworld page'
-    })
-
-//装载所有子路由
-let router = new Router()
-router.use('/', home.routes(), home.allowedMethods())
-router.use('/page', page.routes(), page.allowedMethods())
-
-//加载路由中间件
-app.use(router.routes()).use(router.allowedMethods())
-
 app.listen(3000, () => {
-    console.log('[demo] koa-router is starting on port: 3000')
+    console.log('[demo] get request is starting on port: 3000')
 })
