925d0c5c38dfef82f25d7dc6b3d8027be9ac5c49
dravenxiaokai <jspankai@vip.qq.com>
2017-10-24 22:04:35 +0800

    koa@2路由中间件

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const Koa = require('koa')
-const loggerAsync = require('./middleware/logger-async')
-var app = new Koa()
+const fs = require('fs')
+const app = new Koa()
 
-app.use(loggerAsync())
+const Router = require('koa-router')
 
-app.use((ctx) => {
-    ctx.body = 'hello world'
+//子路由1
+let home = new Router()
+home.get('/', async (ctx) => {
+    let html = `
+        <ul>
+            <li><a href="/page/helloworld">/page/helloworld</a></li>
+            <li><a href="/page/404">/page/404</a></li>
+        </ul>
+    `
+    ctx.body = html
 })
-app.listen(3000, 'localhost', () => {
-    console.log('starting on port: ', 3000)
+
+//子路由2
+let page = new Router()
+page
+    .get('/404', async (ctx) => {
+        ctx.body = '404 page'
+    })
+    .get('/helloworld', async (ctx) => {
+        ctx.body = 'helloworld page'
+    })
+
+//装载所有子路由
+let router = new Router()
+router.use('/', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
+
+//加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(3000, () => {
+    console.log('[demo] koa-router is starting on port: 3000')
 })
