84786ac1d5552bc4375a83e37951f8a9d3b763ee
dravenxiaokai <jspankai@vip.qq.com>
2017-10-25 14:06:29 +0800

    koa@2原生实现JSONP

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const mysql = require('mysql')
+const Koa = require('koa')
+const app = new Koa()
 
-//创建数据连接池
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'koademo'
+app.use(async (ctx) => {
+    //如果JSONP的请求为GET
+    if (ctx.method === 'GET' && ctx.url.split('?')[0] === '/getData.jsonp') {
+        //获取JSONP的callback
+        let callbackName = ctx.query.callback || 'callback'
+        let returnData = {
+            success: true,
+            data: {
+                text: 'this is a jsonp api',
+                time: new Date().getTime()
+            }
+        }
+        //JSONP的script字符串
+        let jsonpStr = `;${callbackName}(${JSON.stringify(returnData)})`
+        //用text/javascript，让请求支持跨域请求
+        ctx.type = 'text/javascript'
+        //输出jsonp字符串
+        ctx.body = jsonpStr
+    } else {
+        ctx.body = 'hello jsonp'
+    }
 })
-
-//在数据池中进行会话操作
-pool.getConnection((err, conn) => {
-    conn.query('SELECT * FROM test', (err, rs, fields) => {
-        //结束会话
-        conn.release()
-
-        if (err) throw err
-    })
+app.listen(3000, () => {
+    console.log('[demo] jsonp is tarting on port 3000')
 })
