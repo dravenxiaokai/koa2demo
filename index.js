32edba6e98b38b9edc83f9e0d678964d5ca44275
dravenxiaokai <jspankai@vip.qq.com>
2017-10-25 18:27:40 +0800

    koa@2 koa-jsonp中间件

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 const Koa = require('koa')
+const jsonp = require('koa-jsonp')
 const app = new Koa()
 
+//使用中间件
+app.use(jsonp())
+
 app.use(async (ctx) => {
-    //如果JSONP的请求为GET
-    if (ctx.method === 'GET' && ctx.url.split('?')[0] === '/getData.jsonp') {
-        //获取JSONP的callback
-        let callbackName = ctx.query.callback || 'callback'
-        let returnData = {
-            success: true,
-            data: {
-                text: 'this is a jsonp api',
-                time: new Date().getTime()
-            }
+    let returnData = {
+        success: true,
+        data: {
+            text: 'this is a jsonp api',
+            time: new Date().getTime()
         }
-        //JSONP的script字符串
-        let jsonpStr = `;${callbackName}(${JSON.stringify(returnData)})`
-        //用text/javascript，让请求支持跨域请求
-        ctx.type = 'text/javascript'
-        //输出jsonp字符串
-        ctx.body = jsonpStr
-    } else {
-        ctx.body = 'hello jsonp'
     }
+    //直接输出json
+    ctx.body = returnData
 })
 app.listen(3000, () => {
-    console.log('[demo] jsonp is tarting on port 3000')
+    console.log('[demo] koa-jsonp is tarting on port 3000')
 })
